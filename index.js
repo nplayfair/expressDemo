c0480bce3e719aa29ed3b6f7d6e9a19729b1895d
Nick Playfair <nick@chaosemerald.co.uk>
2019-05-16 16:57:40 +0100

    Move routes into seperate files

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 const express   = require('express');
 const path      = require('path');
 const logger    = require('./middleware/logger');
-const members   = require('./Members');
+
 
 const app = express();
-
-
 
 // Init middleware
 //app.use(logger);
 
-// Gets all members
-app.get('/api/members', (req, res) => res.json(members));
-
-// Get a single member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-    if (found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-    }
-});
-
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Member API routes
+app.use('/api/members', require('./routes/api/members'));
 
 
 // Start server
