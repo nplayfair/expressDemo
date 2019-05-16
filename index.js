c92be9740a192baa5d6db2d9ca85da5bd8e3ba22
Nick Playfair <nick@chaosemerald.co.uk>
2019-05-16 16:28:58 +0100

    Route to return JSON of members

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const path      = require('path');
 
 const app = express();
 
+// Example data
+const members = [
+    {
+        id: 1,
+        name: 'Nick Playfair',
+        email: 'nick@nickplayfair.co.uk',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'John Smith',
+        email: 'john@mail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Anita Adams',
+        email: 'anita@email.com',
+        status: 'active'
+    }
+]
+
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
